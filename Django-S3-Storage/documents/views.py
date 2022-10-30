ce4437c4880ce74cb2e75c64e9f17ebba63e984e
bekzod818 <bekzod030900@gmail.com>
2022-10-30 22:02:14 +0500

    access

---
+++ Django-S3-Storage/documents/views.py
@@ -0,0 +1,16 @@
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+
+from .models import Document
+
+
+class DocumentCreateView(CreateView):
+    model = Document
+    fields = ['upload', ]
+    success_url = reverse_lazy('home')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        documents = Document.objects.all()
+        context['documents'] = documents
+        return context
